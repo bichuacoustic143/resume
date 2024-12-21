d48dd4f5c8a60b250d40e76a380a9cdffcdd09ab
bichuacoustic143 <53387428+bichuacoustic143@users.noreply.github.com>
2024-12-21 10:07:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'HEAD'])
 def home():
+    if request.method == 'HEAD':
+        return '', 200  # Respond to HEAD requests with an empty response
     return render_template('index.html')
 
 @app.route('/download')
